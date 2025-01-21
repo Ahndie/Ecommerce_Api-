d41eaf21f7540f762a5032ee88b27a8a11017dec
Ahndie <andrewbamfo@gmail.com>
2025-01-21 07:45:49 +0000

    completed API

--- Ecommerce_API/settings.py
+++ Ecommerce_API/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^b9bh!ncse3=!rr_-t0#ii@^j-i3!w0!c+-kqi3dsj+o*hf(39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
